938f14345ad05d514fa471f32f794ab6b44505c1
Deeptixcdm <deepti@xcdm.world>
2025-04-22 13:05:18 +0530

    newchange

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/StyleNest/' // For direct hosting on GitHub Pages, without a subfolder.
+  base: '/styleNest/' // For direct hosting on GitHub Pages, without a subfolder.
 })
